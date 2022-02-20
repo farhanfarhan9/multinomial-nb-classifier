ebfee92f8b339ab03fd6f9d84b7e2e32633b3375
Farhan <faarhan7@gmail.com>
2022-02-20 19:54:23 +0700

    classification

---
+++ classification/NbModel.py
@@ -0,0 +1,165 @@
+#import Library
+import re
+import pandas as pd
+import time
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import metrics
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn import svm
+from sklearn.naive_bayes import MultinomialNB
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+class NbModel():
+    #constructor
+    def __init__(self, datasetPath):
+        self.classifier = None
+        self.tf_vect = None
+
+        self.dataset = pd.read_csv(datasetPath)
+
+        # self.label = dataset.label      #set kolom label pd dataset ke dalam variabel
+        
+        # self.dataset = dataset.drop("label", axis = 1)      #drop kolom label pada dataset
+
+    #fungsi untuk train_test_split pada scikit-learn
+    def variabel(self):
+        #split dataset, 6/10 menjadi data latih, 4/10 menjadi data uji
+        perkara_train, perkara_test, pasal_train, pasal_test = train_test_split(self.dataset['kronologi'], self.dataset['pasal'], test_size=0.2, random_state=32)
+        return perkara_train, perkara_test, pasal_train, pasal_test
+
+    #fungsi untuk vectorizing split_data with TFIDF
+    def split_data(self):
+        self.tf_vect = TfidfVectorizer()
+        self.text_tf = self.tf_vect.fit_transform(self.dataset['kronologi'].astype('U'))
+    #         tf_vect.fit(perkara['kronologi'])
+    #     perkara_train, perkara_test, pasal_train, pasal_test = variabel()
+        perkara_train_tf, perkara_test_tf, pasal_train, pasal_test = train_test_split(self.text_tf, self.dataset['pasal'], test_size=0.2, random_state=32)
+
+        #ubah dataset menjadi vector)
+        return perkara_train_tf, perkara_test_tf, pasal_train, pasal_test
+        # ----------------------------
+
+
+        # self.tf_vect = TfidfVectorizer()
+        # self.tf_vect.fit(self.dataset['kronologi'])
+        # perkara_train, perkara_test, pasal_train, pasal_test = self.variabel()
+
+        # #ubah dataset menjadi vector
+        # perkara_train_tf = self.tf_vect.fit_transform(perkara_train)
+        # perkara_test_tf = self.tf_vect.transform(perkara_test)
+        # return perkara_train_tf, perkara_test_tf, pasal_train, pasal_test
+
+        
+
+    #fungsi untuk vectorizing data uji
+    def vectorizer_data_test(self, datatestPath):
+        return self.tf_vect.transform(datatestPath)
+    
+    #fungsi untuk klasifikasi svm>ganti jadi nb
+    def nb(self, perkara_train_tf, pasal_train):
+        self.classifier = MultinomialNB()
+        self.classifier.fit(perkara_train_tf, pasal_train)
+    
+    #fungsi untuk prediksi data uji
+    def predict(self, nb_test):
+        return self.classifier.predict(nb_test)
+
+    #fungsi untuk skor akurasi
+    def akurasi(self, y_asli, y_output):
+        return metrics.accuracy_score(y_asli, y_output)
+
+    #fungsi untuk nampilin klasifikasi report (precission, recall, f1-score, support)
+    def classi_report(self, y_test, y_output):
+        return classification_report(y_test, y_output)
+
+    def conf_matrix(self, y_test, y_output):
+        return confusion_matrix(y_test, y_output)
+
+    #fungsi untuk preprocessing data testing
+    def read_split(self, datatestPath):
+        with open(datatestPath, "r",encoding="utf8") as file:
+            test_file = file.read()
+            file.close()
+
+        #casefolding menjadi huruf kecil
+        text_low = test_file.lower().strip()
+
+
+
+        #cleaning text
+        re_clean = re.sub('(@[A-Za-z0-9]+)|(#{A-Za-20-9]+)|(\w+\/\/\S+)|(http\S+)','',text_low)
+        re_clean = re.sub(r'_', '', re_clean) # menghapus _
+        re_clean = re.sub(r'/', '  ', re_clean) #menggantikan / menjadi spasi
+        re_clean = re.sub(r'\d+', '', re_clean) #menghapus angka
+        re_clean = re.sub(r'\n', ' ', re_clean) # untk mengubah enter menjadi spasi
+        re_clean = re.sub(r'[^A-Za-z\s\/]' ,' ', re_clean) #menghapus karakter yang bukan huruf
+
+        stop_words=set(stopwords.words('indonesian'))
+        stop_words.update(['terdakwa','twitter','fb','facebook','status','mengupload','memposting','wa','bbm','saksi','blackberry','messenger','januari','februari','maret','april','mei','juni','juli','agustus','september','oktober','november','desember','media','sosial','medsos','tanggal'])
+        # stop_words.update(['januari','februari','maret','april','mei','juni','juli','agustus','september','oktober','november','desember','media','sosial','medsos','tanggal'])
+        no_stopwords = [word for word in re_clean.split() if word not in stop_words]
+        no_step = ' '.join(no_stopwords)
+        # return no_step
+
+        factory = StemmerFactory()
+        stemmer = factory.create_stemmer()
+        return [stemmer.stem(no_step)]
+
+        sentence = re_clean.split(" ")
+        # print(re_clean)
+        
+        new_text = ""
+        for i in sentence:
+            new_text += i+" "
+        a = new_text
+        b = a.split(".")
+        del b[-1]
+        return b
+
+    def casefolding(perkara):
+        perkara = perkara.lower().strip()
+        return perkara
+
+    def clean(perkara):
+        perkara = ''.join(re.sub("(@[A-Za-z0-9]+)|(#{A-Za-20-9]+)|(\w+\/\/\S+)|(http\S+)", "", perkara)) #hapus
+
+        perkara = re.sub(r'_', '', perkara) # menghapus _
+        perkara = re.sub(r'/', '  ', perkara) #menggantikan / menjadi spasi
+        perkara = re.sub(r'\d+', '', perkara) #menghapus angka
+        perkara = re.sub(r'\n', ' ', perkara) # untk mengubah enter menjadi spasi
+        perkara = re.sub(r'[^A-Za-z\s\/]' ,' ', perkara) #menghapus karakter yang bukan huruf
+        return perkara
+
+    def remove_stopwords(perkara):
+        stop_words=set(stopwords.words('indonesian'))
+        stop_words.update(['terdakwa','twitter','fb','facebook','status','mengupload','memposting','wa','bbm','saksi','blackberry','messenger','januari','februari','maret','april','mei','juni','juli','agustus','september','oktober','november','desember','media','sosial','medsos','tanggal'])
+    # stop_words.update(['januari','februari','maret','april','mei','juni','juli','agustus','september','oktober','november','desember','media','sosial','medsos','tanggal'])
+        no_stopwords = [word for word in perkara.split() if word not in stop_words]
+        no_step = ' '.join(no_stopwords)
+        return no_step
+
+    def stemmer(perkara):
+        factory = StemmerFactory()
+        stemmer = factory.create_stemmer()
+        return stemmer.stem(perkara)
+
+
+    #fungsi untuk proses prediksi data uji
+    def ProcessingText(self,datatestPath):
+        sentence_vect = self.vectorizer_data_test(datatestPath)
+        prediksi_test = self.predict(sentence_vect)
+        dataHasil = {}
+        # result = pd.DataFrame({'Kalimat' : datatestPath, 'Label' : prediksi_test})
+        for i in range(len(prediksi_test)):
+            dataHasil = {
+                'kalimat':datatestPath,
+                'label':prediksi_test
+            }
+        # print(dataHasil["label"])
+        return [dataHasil]
+
+#Main
+if __name__=='__main__':
+    print("berhasil")
---
+++ index.py
@@ -0,0 +1,109 @@
+from flask import Flask, render_template, jsonify, request, Response, send_file
+from werkzeug.utils import secure_filename
+from classification.NbModel import NbModel
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+import json, os, csv
+import pandas as pd
+
+UPLOAD_FOLDER = 'data/'
+ALLOWED_EXTENSIONS = set(['json'])
+ALLOWED_EXTENSIONS_TXT = set(['txt'])
+ALLOWED_EXTENSIONS_SVM = set(['csv'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def allowed_file_txt(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_TXT
+
+def allowed_file_svm(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS_SVM
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/training')
+def training():
+    return render_template('training.html')
+
+@app.route('/training_result', methods=['POST', 'GET'])
+def training_result():
+    error = ""
+    if 'file_dataset' in request.files:
+        filetext = request.files["file_dataset"]
+        print(filetext)
+        filetext1 = request.files["file_dataset"]
+        if filetext and allowed_file_svm(filetext.filename):
+            filename = secure_filename(filetext.filename)
+            filetext.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            dataset = pd.read_csv('data/' + filetext.filename)
+        else:
+            error = "Format file salah !"
+
+    nbModelPath = 'data/' + filetext.filename
+
+    obj = NbModel(nbModelPath)
+    # split_p = obj.split_data()
+    perkara_train_tf, perkara_test_tf, pasal_train, pasal_test = obj.split_data()
+    obj.nb(perkara_train_tf, pasal_train)
+
+    prediksi_dataset = obj.predict(perkara_test_tf)
+    akurasi_dataset = obj.akurasi(pasal_test, prediksi_dataset) * 100
+
+    table = classification_report(pasal_test, prediksi_dataset)
+    split_tab = table.split()
+
+    data_classi_report = [akurasi_dataset, split_tab]
+
+    return render_template ('datatable_classification_result.html', data = data_classi_report)
+
+@app.route('/uji_dokumen')
+def test_classification_result():
+    return render_template('/testing_dokumen.html')
+
+
+@app.route('/testing_document_result', methods=['POST', 'GET'])
+def hasil_uji_dokumen():
+    error = ""
+    if 'file_testing' in request.files:
+        filetext = request.files["file_testing"]
+        # print(filetext)
+        filetext = request.files["file_testing"]
+        if filetext and allowed_file_txt(filetext.filename):
+            filename = secure_filename(filetext.filename)
+            filetext.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], filetext.filename), 'r', encoding='utf-8') as f:
+                text = f.read()
+                f.close()
+        else:
+            error = "Format file salah !"
+
+    datatestPath = 'data/' + filetext.filename
+    nbModelPath = 'data/data_pasal_bersih.csv'
+
+
+    objTest = NbModel(nbModelPath)
+    # split_p = objTest.split_data()
+    cerita_train_tf, cerita_test_tf, label_train, label_test = objTest.split_data()
+    objTest.nb(cerita_train_tf, label_train)
+    read_file = objTest.read_split(datatestPath)
+
+    hasilnya = objTest.ProcessingText(read_file)
+
+    penampung = []
+
+    # return "berhasil"
+    return render_template('datatable_test_result.html', data=hasilnya)
+
+if __name__=="__main__":
+    app.run(debug = True)
+
+    app.run()
